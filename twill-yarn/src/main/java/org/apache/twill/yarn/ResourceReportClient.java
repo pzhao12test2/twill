501df149b069bb6015643e4422f53b18f4e08bb5
DrRacket <zpy1995@gmail.com>
2017-12-09 11:35:42 +0000

    e61be38fb071 replay gib

--- twill-yarn/src/main/java/org/apache/twill/yarn/ResourceReportClient.java
+++ twill-yarn/src/main/java/org/apache/twill/yarn/ResourceReportClient.java
@@ -17,21 +17,17 @@
  */
 package org.apache.twill.yarn;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Closeables;
 import org.apache.twill.api.ResourceReport;
 import org.apache.twill.internal.json.ResourceReportAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.zip.DeflaterInputStream;
-import java.util.zip.GZIPInputStream;
 
 /**
  * Package private class to get {@link ResourceReport} from the application master.
@@ -40,10 +36,10 @@ final class ResourceReportClient {
   private static final Logger LOG = LoggerFactory.getLogger(ResourceReportClient.class);
 
   private final ResourceReportAdapter reportAdapter;
-  private final List<URL> resourceUrls;
+  private final URL resourceUrl;
 
-  ResourceReportClient(List<URL> resourceUrls) {
-    this.resourceUrls = resourceUrls;
+  ResourceReportClient(URL resourceUrl) {
+    this.resourceUrl = resourceUrl;
     this.reportAdapter = ResourceReportAdapter.create();
   }
 
@@ -52,40 +48,16 @@ final class ResourceReportClient {
    * @return A {@link ResourceReport} or {@code null} if failed to fetch the report.
    */
   public ResourceReport get() {
-    for (URL url : resourceUrls) {
+    try {
+      Reader reader = new BufferedReader(new InputStreamReader(resourceUrl.openStream(), Charsets.UTF_8));
       try {
-        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-        urlConn.setRequestProperty("Accept-Encoding", "gzip, deflate");
-
-        if (urlConn.getResponseCode() != 200) {
-          continue;
-        }
-
-        try (Reader reader = new InputStreamReader(getInputStream(urlConn), StandardCharsets.UTF_8)) {
-          LOG.trace("Report returned by {}", url);
-          return reportAdapter.fromJson(reader);
-        }
-      } catch (IOException e) {
-        // Just log a trace as it's ok to not able to fetch resource report
-        LOG.trace("Exception raised when getting resource report from {}.", url, e);
+        return reportAdapter.fromJson(reader);
+      } finally {
+        Closeables.closeQuietly(reader);
       }
+    } catch (Exception e) {
+      LOG.error("Exception getting resource report from {}.", resourceUrl, e);
+      return null;
     }
-    return null;
-  }
-
-  private InputStream getInputStream(HttpURLConnection urlConn) throws IOException {
-    InputStream is = urlConn.getInputStream();
-    String contentEncoding = urlConn.getContentEncoding();
-    if (contentEncoding == null) {
-      return is;
-    }
-    if ("gzip".equalsIgnoreCase(contentEncoding)) {
-      return new GZIPInputStream(is);
-    }
-    if ("deflate".equalsIgnoreCase(contentEncoding)) {
-      return new DeflaterInputStream(is);
-    }
-    // This should never happen
-    throw new IOException("Unsupported content encoding " + contentEncoding);
   }
 }
